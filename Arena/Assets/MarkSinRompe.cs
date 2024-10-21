78b0da27172c8c95df9125f6f276d0c140499e2d
ApikGamBom <118607355+ApikGamBom@users.noreply.github.com>
2024-10-21 11:56:29 +0200

    Markmarkmarkmark
    aaaaa

---
+++ Arena/Assets/MarkSinRompe.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MarkSinRompe : MonoBehaviour
+{
+    [SerializeField] private GameObject markSinRompeKube;
+    [SerializeField] private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.useGravity = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+}
